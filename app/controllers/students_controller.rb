e7fba09a609232074171a62be1fb636bc29a3a5f
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-01-29 03:38:54 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action: find_student
 
   def new
     @student = Student.new
@@ -11,15 +12,15 @@ class StudentsController < ApplicationController
 
 
   def show
-    @student = Student.find(params[:id])
+
   end
 
   def edit
-    @student = Student.find(params[:id])
+
   end
 
   def update
-    @student = Student.find(params[:id])
+
     @student.update(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to student_path(@student)
   end
